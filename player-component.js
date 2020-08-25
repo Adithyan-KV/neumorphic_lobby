0730460ea21a274c2ecf32f44e7ab359f44a4241
Adithyan-KV <adithyankv007@gmail.com>
2020-08-26 01:46:55 +0530

    Players can now be removed
     -The remove button was made functional

--- player-component.js
+++ player-component.js
@@ -12,6 +12,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
         connectedCallback() {
             console.log("Added to DOM");
+
+            let remove_button = this.shadowRoot.querySelector("#deny");
+            remove_button.addEventListener('click', () => {
+                let parent_box = this.shadowRoot.querySelector('.player-box');
+                parent_box.remove()
+                console.log(parent_box)
+            })
         }
 
         disconnectedCallback() {
